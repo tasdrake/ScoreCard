59e1037695cf87e671f4fbb9435b851ec8c4e69a
Tasman Terzakes Drake <tdrake@mouthwateringmedia.com>
2018-04-08 14:34:53 -0600

    Basic column structure

--- Components/Home.js
+++ Components/Home.js
@@ -1,33 +1,94 @@
 import React, { Component } from 'react'
 import styles from '../css/Home'
 import {
-  Platform,
-  Text,
-  View
+    View,
+    TextInput 
 } from 'react-native'
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      players: [
+        {
+          name: 'Tas',
+          points: 0
+        },
+        {
+          name: 'Ali',
+          points: 0
+        },
+        {
+          name: 'Emmy',
+          points: 0
+        },
+        {
+          name: 'Wade',
+          points: 0
+        }
+      ],
+      columns: 4
+    }
+  }
+
+  columns = () => {
+    const col = []
+    for (let i = 0; i < this.state.columns; i++) {
+      col.push(<TextInput key={i}
+                          textAlign={'center'}
+                          style={
+                            i === 0 ? [styles.points, styles.firstColumn] :
+                            i === this.state.colums + 1 ? [styles.points, styles.lastColumn] :
+                            styles.points
+                          }>val</TextInput>)
+    }
+    return <View style={styles.pointContainer}>{ col }</View>
+  }
+
+  columnsTop = () => {
+    const col = []
+    for (let i = 0; i < this.state.columns; i++) {
+      col.push(<TextInput key={i}
+                          textAlign={'center'}
+                          style={
+                            i === 0 ? [styles.points, styles.firstColumn] :
+                            styles.points
+                          }>val</TextInput>)
+    }
+    return <View style={styles.pointContainer}>{ col }</View>
+  }
+
   render() {
+    const nameWidth = 75
+    const pointWidth = 50
+    const rowWidth = nameWidth + pointWidth * (this.state.columns - 1)
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit Home.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <View style={styles.container}>
+          {/* <View style={[styles.row, styles.info]}>
+            <Text style={styles.name}>Name</Text>
+            {
+              this.columnsTop()// this.state.columns.map((e, i) => <Text key={i}></Text>)
+            }
+          </View> */}
+          {
+            this.state.players.map((e, i) => {
+              return (
+                <View key={i}
+                      style={
+                        i === 0 ? [styles.row, styles.firstRow, { width: rowWidth }] :
+                        i === this.state.players.length - 1 ? [styles.row, styles.lastRow, { width: rowWidth }] :
+                        [styles.row, { width: rowWidth }]
+                      }>
+                  <TextInput style={styles.name}>{this.state.players[i].name}</TextInput>
+                  {
+                    this.columns()
+                  }
+                </View>
+              )
+            })
+          }
       </View>
-    );
+    )
   }
 }
--- css/Home.js
+++ css/Home.js
@@ -4,18 +4,60 @@ import colors from './colors'
 module.exports = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    backgroundColor: colors.white,
+    padding: 10
+  },
+  name: {
+    backgroundColor: colors.gray,
+    width: 75,
+    textAlign: 'left',
+    paddingLeft: 5,
+    paddingVertical: 5,
+    alignSelf: 'center',
+    height: '100%',
+    borderRightWidth: 1,
+    borderColor: colors.black
+  },
+  row: {
+    borderColor: colors.black,
+    borderRightWidth: 1,
+    borderLeftWidth: 1,
+    borderBottomWidth: 1,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems:'flex-start',
+    maxHeight: 30
+  },
+  firstRow: {
+    borderWidth: 1,
+  },
+  points: {
+    borderLeftWidth: 1,
+    paddingVertical: 6,
+    borderColor: colors.black,
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: colors.white,
+    width: 20,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  pointContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignSelf: 'center',
+    justifyContent: 'center',
   },
-  instructions: {
-    textAlign: 'center',
-    color: colors.black,
-    marginBottom: 5,
+  info: {
+    backgroundColor: colors.gray
   },
-});
+  lastColumn: {
+    borderRightWidth: 0,
+    width: 20,
+  },
+  firstColumn: {
+    borderRightWidth: 0,
+    borderLeftWidth: 0,
+    width: 20,
+  }
+})
